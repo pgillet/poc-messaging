3401f0af40249d3ee9162ce9c1d8cb93a90e1dd9
Pascal Gillet <pascal.gillet@stack-labs.com>
2019-09-03 19:39:51 +0200

    Add clustering

--- centos7-rabbitmq-server/rabbitmq-server-install.sh
+++ centos7-rabbitmq-server/rabbitmq-server-install.sh
@@ -2,6 +2,12 @@
 
 # sudo-enabled user on the VM: vagrant/vagrant
 
+HOSTNAME=$(hostname --short)
+FQDN=$(hostname --fqdn)
+
+# Determine the BIOS vendor
+VENDOR=$(dmidecode -s bios-vendor)
+
 # See https://www.rabbitmq.com/install-rpm.html
 
 # Install pygpgme, a package which allows yum to handle gpg signatures, 
@@ -79,21 +85,63 @@ cat <<EOM > /etc/systemd/system/rabbitmq-server.service.d/limits.conf
 LimitNOFILE=64000
 EOM
 
+# Enable plugins
+#echo "[rabbitmq_management,rabbitmq_mqtt,rabbitmq_peer_discovery_consul]." | sudo tee /etc/rabbitmq/enabled_plugins
+echo "[rabbitmq_management,rabbitmq_mqtt,rabbitmq_peer_discovery_consul]." > /etc/rabbitmq/enabled_plugins
+
+# Clustering - Peer discovery using Consul
+cat <<EOM > /etc/rabbitmq/rabbitmq.conf
+cluster_formation.peer_discovery_backend = rabbit_peer_discovery_consul
+cluster_formation.consul.host = consul
+EOM
+
+# Erlang cookie
+# Shared secret that must be present on all cluster nodes for inter-node network connectivity and authentication
+# TODO: Use Consul key-value store to manage the Erlang cookie?
+
+if [ "${VENDOR}" = Google ]; then
+	# Google Cloud Platform
+
+	METADATA_REQUEST=http://metadata.google.internal/computeMetadata/v1/instance/attributes/ERLANG_COOKIE
+	HTTP_CODE=$(curl --write-out %{http_code} --output /dev/null -s -H "Metadata-Flavor: Google" ${METADATA_REQUEST})
+	if [ "${HTTP_CODE}" = 200 ]; then
+		ERLANG_COOKIE=$(curl -s -H "Metadata-Flavor: Google" ${METADATA_REQUEST})
+	fi
+fi
+
+if [[ -n "${ERLANG_COOKIE}" ]]; then
+	echo -n ${ERLANG_COOKIE} > /var/lib/rabbitmq/.erlang.cookie
+fi
+
 # Start the server
 chkconfig rabbitmq-server on
 /sbin/service rabbitmq-server start
 
-# Access control
-# Create a RabbitMQ user called "meteofr"
-rabbitmqctl add_user meteofr meteofr
 
-# Create a new virtual host called "test"
-rabbitmqctl add_vhost test
+if [[ "${HOSTNAME}" == rabbitmq-server-01 ]]; then 
+	# Access control
+	# Create a RabbitMQ user called "meteofr"
+	rabbitmqctl add_user meteofr meteofr
 
-# Grant the user named "meteofr" access to the virtual host called "test", 
-# with configure permissions on all resources whose names starts with "meteofr-", 
-# and write and read permissions on all resources
-rabbitmqctl set_permissions -p test meteofr "^meteofr-.*" ".*" ".*"
+	# Create a new virtual host called "test"
+	rabbitmqctl add_vhost test
+
+	# Grant the user named "meteofr" access to the virtual host called "test", 
+	# with configure permissions on all resources whose names starts with "meteofr-", 
+	# and write and read permissions on all resources
+	rabbitmqctl set_permissions -p test meteofr "^meteofr-.*" ".*" ".*"
+
+	# Tag the user with "administrator" for full management UI and HTTP API access
+	rabbitmqctl set_user_tags meteofr administrator
+
+	# Delete the defaut guest user as a primary security measure
+	rabbitmqctl delete_user guest
+else 
+	# Clustering
+	rabbitmqctl stop_app
+	rabbitmqctl join_cluster rabbit@rabbitmq-server-01
+	rabbitmqctl start_app
+fi
 
 # Check on service status as observed by service manager
 service rabbitmq-server status
@@ -103,5 +151,3 @@ service rabbitmq-server status
 #curl https://storage.googleapis.com/compute-image-tools/release/linux/import_precheck --output import_precheck
 #chmod u+x import_precheck
 #./import_precheck
-
-
