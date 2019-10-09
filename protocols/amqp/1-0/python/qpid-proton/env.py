7e31687cad25f51ed906d2fcd52dff01fe2b3879
Pascal Gillet <pascal.gillet@stack-labs.com>
2019-10-09 12:21:50 +0200

    Add failover test with Qpid Proton python client

--- protocols/amqp/1-0/python/qpid-proton/env.py
+++ protocols/amqp/1-0/python/qpid-proton/env.py
@@ -1,5 +1,8 @@
 # ActiveMQ Artemis
 server_addr = '35.195.38.29:5672/examples'
 
+# Failover with a list of URL strings of process to try to connect to.
+server_addr = ["amqp://host1:5672", "amqp://host2:5672", "amqp://host3:5672"]
+
 # RabbitMQ
 #server_addr = '34.76.147.112:5672/examples'
