891c3cfb268af9bdf28087ef1b1bb8763cfbc2d4
Pascal Gillet <pascal.gillet@stack-labs.com>
2020-01-07 17:58:32 +0100

    Add Python client for new performance tests

--- protocols/amqp/1-0/python/qpid-proton/env.py
+++ protocols/amqp/1-0/python/qpid-proton/env.py
@@ -1,8 +1,17 @@
 # ActiveMQ Artemis
-server_addr = '35.195.38.29:5672/examples'
+# server_addr = '35.189.241.214:5672/examples'
 
 # Failover with a list of URL strings of process to try to connect to.
-server_addr = ["amqp://host1:5672", "amqp://host2:5672", "amqp://host3:5672"]
+#server_addr = ["amqps://35.241.200.129:5671", "amqps://35.241.250.148:5671", "amqps://34.76.223.238:5671"]
 
 # RabbitMQ
-#server_addr = '34.76.147.112:5672/examples'
+server_addr = ["amqps://34.77.53.43:5671", "amqps://35.195.128.110:5671", "amqps://34.76.93.87:5671"]
+
+# Destination name
+address = 'jms.queue.PerfQueue'
+
+# TLS/SSL
+certificate_db = '/home/pascal/Workbench/Projects/MeteoFrance/tls-gen/basic/tls/ca_certificate.pem'
+cert_file = '/home/pascal/Workbench/Projects/MeteoFrance/tls-gen/basic/tls/client_certificate.pem'
+key_file = '/home/pascal/Workbench/Projects/MeteoFrance/tls-gen/basic/tls/client_key.pem'
+password = 'bunnies'
